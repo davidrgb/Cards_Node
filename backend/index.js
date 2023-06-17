646646becc9213da0657292e04ac0443091c800e
David Russell <davidrussellrgb@gmail.com>
2023-06-16 21:58:52 -0500

    Connected to MySQL database

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,17 @@ const app = express();
 
 const port = 3010;
 
+const mysql = require('mysql');
+
+const credentials = require('./credentials.json');
+
+const connection = mysql.createConnection({
+  host: credentials.host,
+  user: credentials.user,
+  password: credentials.password,
+  database: credentials.database,
+});
+
 app.use(express.static(path.resolve(__dirname, '../cards/build')));
 
 app.get('/api/test', (req, res) => {
@@ -18,3 +29,8 @@ app.get('*', (req, res) => {
 app.listen(port, () =>
   console.log(`Cards listening on port ${port}!`),
 );
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database!");
+});
