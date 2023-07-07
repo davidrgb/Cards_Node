9510e102bf2e5f610789e345b92fbd59463c0bcb
David Russell <davidrussellrgb@gmail.com>
2023-07-07 16:57:21 -0500

    Added redirect from login when authenticated

--- cards/src/pages/Login.js
+++ cards/src/pages/Login.js
@@ -2,8 +2,28 @@ import './Login.css';
 
 import LoginForm from '../components/LoginForm';
 
+import { useNavigate } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
+
 export default function Login() {
-    return (
+    const navigate = useNavigate();
+    const [authenticated, setAuthenticated] = useState();
+
+    const requestOptions = {
+        method: 'GET',
+    };
+
+    useEffect(() => {
+        const getAuthenticated = async () => await fetch('/session', requestOptions)
+            .then(response => {
+                setAuthenticated(response.redirected === false);
+            });
+        getAuthenticated();
+        if (authenticated !== undefined && authenticated !== null && authenticated) navigate('/');
+    });
+
+    return authenticated === false ? (
         <div className='login'>
             <div className='stack'>
                 <div className='card left'></div>
@@ -13,5 +33,5 @@ export default function Login() {
             <h1 className='fade first-fade'>Cards</h1>
             <LoginForm />
         </div>
-    );
+    ) : <div></div>;
 }
